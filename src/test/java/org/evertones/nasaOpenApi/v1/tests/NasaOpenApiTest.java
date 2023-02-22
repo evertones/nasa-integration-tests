77c29658a873e2e12687599ae9dc67d12f00a55a
Everton Eckert Schneider <everton.schneider@gmail.com>
2023-02-22 15:14:04 -0300

    Update test to use classes mapped to get the NASA API response.

--- src/test/java/org/evertones/nasaOpenApi/v1/tests/NasaOpenApiTest.java
+++ src/test/java/org/evertones/nasaOpenApi/v1/tests/NasaOpenApiTest.java
@@ -1,31 +1,30 @@
 package org.evertones.nasaOpenApi.v1.tests;
 
-import org.evertones.nasaOpenApi.v1.apiProperties.MarsRoverCamera;
 import org.evertones.nasaOpenApi.v1.apiProperties.MarsRoverMartianSolParameter;
+import org.evertones.model.MarsPhotos;
 import org.junit.Test;
 
-import java.net.HttpURLConnection;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.everyItem;
-import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
 
 public class NasaOpenApiTest
     extends NasaOpenApiFixture {
 
     @Test
     public void testGet10FirstPhotosByCuriosityOn1000Sol() {
+        MarsPhotos marsPhotos = given()
+                    .param(MarsRoverMartianSolParameter.API_KEY.getValue(), getNasaApiKey())
+                    .param(MarsRoverMartianSolParameter.SOL.getValue(), "1000")
+                    .param(MarsRoverMartianSolParameter.PAGE.getValue(), "1")
+                .when().log().all()
+                .get(getMarsPhotosApiUrl().toString())
+                .as(MarsPhotos.class);
 
-        given()
-            .param(MarsRoverMartianSolParameter.API_KEY.getValue(), getNasaApiKey())
-            .param(MarsRoverMartianSolParameter.SOL.getValue(), "1000")
-            .param(MarsRoverMartianSolParameter.PAGE.getValue(), "1")
-        .when()
-        .log().all()
-        .get(getMarsPhotosApiUrl().toString())
-        .then()
-            .statusCode(HttpURLConnection.HTTP_OK);
+        marsPhotos.getPhotos().forEach(photo -> {
+            System.out.println(photo.toString());
+        });
+
+        assertEquals(marsPhotos.getPhotos().size(), 25);
     }
 
 }
